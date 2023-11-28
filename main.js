66c3103cc28c7a484b6d1277908fc1087a21f872
Thiago <Thiago.a.rostomashvili@gmail.com>
2023-11-28 19:29:07 +0000

    adicionado secção de contato ao projeto

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
-let search = document.querySelector('.search-box');
+let search = document.querySelector(".search-box");
 
-document.querySelector('#search-icon').onclick = () => {
-        search.classList.toggle('active');
-        navbar.classList.remove('active');
-}
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  navbar.classList.remove("active");
+};
 
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector(".navbar");
 
-document.querySelector('#menu-icon').onclick = () => {
-        navbar.classList.toggle('active');
-        search.classList.remove('active');
-}
+document.querySelector("#menu-icon").onclick = () => {
+  navbar.classList.toggle("active");
+  search.classList.remove("active");
+};
 
 window.onscroll = () => {
-        navbar.classList.remove('active');
-        search.classList.remove('active');
-}
+  navbar.classList.remove("active");
+  search.classList.remove("active");
+};
 
+let header = document.querySelector("header");
 
-let header = document.querySelector('header');
-
-window.addEventListener('scroll' , () => {
-        header.classList.toggle('shadow', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
 });
+
+function saveContact() {
+  // Get the forms values
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+
+  // To creat the object to represent the contact
+  var contact = {
+    name: name,
+    email: email,
+  };
+
+  // Convert the object into a JSon String
+  var contactString = JSON.stringify(contact);
+
+  // To save the string JSON on localStorage
+  localStorage.setItem("contact", contactString);
+
+  // Clear the form fields after saving
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+
+  alert("Contact saved successfully!");
+}
